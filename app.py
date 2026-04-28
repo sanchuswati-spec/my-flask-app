dcd45611568f1b0cc20d1ff5b41b72accef3b21a
Ubuntu <ubuntu@ip-172-31-43-65.eu-north-1.compute.internal>
2026-04-28 07:33:02 +0000

    Final Docker CI/CD Test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-	return "Docker Safe Deploy!!! "
+	return "Docker Final Working!!! "
 
 if __name__ =='__main__':
 	app.run(host='0.0.0.0', port=5000)
