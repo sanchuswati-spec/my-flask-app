94813d90a4a45a0b252a07490db2045db2acfd9a
Ubuntu <ubuntu@ip-172-31-43-65.eu-north-1.compute.internal>
2026-04-28 07:55:38 +0000

    Test CI/CD marker

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-	return "CI/CD Final!!! "
+	return "CI/CD Marker test!! "
 
 if __name__ =='__main__':
 	app.run(host='0.0.0.0', port=5000)
