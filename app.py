80bcf77587b91de725eec1694f907e0f588f5da4
Ubuntu <ubuntu@ip-172-31-43-65.eu-north-1.compute.internal>
2026-04-28 08:26:06 +0000

    trigger ci cd

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-	return "CI/CD Marker test!! "
+	return "CI/CD Final Deploy!! "
 
 if __name__ =='__main__':
 	app.run(host='0.0.0.0', port=5000)
